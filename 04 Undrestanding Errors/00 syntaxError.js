2d1e5db868e5ffefda2e6a0402b0d822747f214b
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-07-05 11:30:19 +0500

    syntax Errors in javascript

---
+++ 04 Undrestanding Errors/00 syntaxError.js
@@ -0,0 +1,6 @@
+// syntax errors are errors caused by miss spilling or missing a simicolon, or a curly braces etc
+// the problem is the compiler will just return an undefined error occured but not tells you what is the error
+
+const x = 10; //this is okay
+//cons  y= 20;   // SyntaxError: Unexpected identifier 'y'
+//actually i missed a t in the spelling
