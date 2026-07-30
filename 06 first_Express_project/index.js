ac38b917e9c45c44f8f0dd98e90ed40bba26eaa6
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-07-30 19:12:30 +0500

    added a little fic to idex.js

--- 06 first_Express_project/index.js
+++ 06 first_Express_project/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const blogs = require('./data/blogs');
+const express = require("express");
+const path = require("path");
+const blogs = require("./data/blogs");
 
 const app = express();
 const PORT = 3000;
@@ -8,11 +8,11 @@ const PORT = 3000;
 // ===== MIDDLEWARE =====
 
 // 1. Serve static files from 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // 2. Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // 3. Parse URL-encoded data (for forms)
 app.use(express.urlencoded({ extended: true }));
@@ -23,43 +23,43 @@ app.use(express.json());
 // ===== ROUTES =====
 
 // Homepage - List all blogs
-app.get('/', (req, res) => {
-  res.render('blogs', { 
+app.get("/", (req, res) => {
+  res.render("blogs", {
     blogs: blogs,
-    title: 'All Blogs'
+    title: "All Blogs",
   });
 });
 
 // Single blog page
-app.get('/blog/:id', (req, res) => {
+app.get("/blog/:id", (req, res) => {
   const blogId = parseInt(req.params.id);
-  const blog = blogs.find(b => b.id === blogId);
-  
+  const blog = blogs.find((b) => b.id === blogId);
+
   if (blog) {
-    res.render('blog', { 
+    res.render("blog", {
       blog: blog,
-      title: blog.title
+      title: blog.title,
     });
   } else {
-    res.status(404).send('Blog not found!');
+    res.status(404).send("Blog not found!");
   }
 });
 
 // About page
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 // Contact page (example of serving HTML)
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
 });
 
 // ===== ERROR HANDLING =====
 
 // 404 - Page not found
 app.use((req, res) => {
-  res.status(404).send('404 - Page Not Found');
+  res.status(404).send("404 - Page Not Found");
 });
 
 // ===== START SERVER =====
@@ -67,3 +67,5 @@ app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
   console.log(`📝 View blogs at http://localhost:${PORT}`);
 });
+
+module.exports = app;
